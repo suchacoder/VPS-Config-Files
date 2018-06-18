4c8c41d292cf3c2b054271744be320d3d6f9b6cb
suchacoder <leanhack@gmail.com>
2018-06-17 21:03:42 -0300

    Added killgual.sh

---
+++ bin/killgual.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Resseting iptables firewall to default and allowing everything..."
+
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -t raw -F
+iptables -t raw -X
+iptables -t security -F
+iptables -t security -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
