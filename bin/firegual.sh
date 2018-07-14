f0e45f0d809505436adad201a4151ea2dc45999b
suchacoder <leanhack@gmail.com>
2018-07-14 13:45:06 -0300

    Updated firegual

--- bin/firegual.sh
+++ bin/firegual.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-# Jim McKibben
 # SSH Port easy customization
 # Allows Local Loopback
 # Allows DNS Query and Response
 # Blocks bad source
 # Blocks non local Loopback
+# Blocks port scanners
+# Blocks spoofed/invalid packets
+# Blocks Smurf attacks
 # DOS Protection and reporting
 # DOS SYN Flood
 # DOS ICMP
@@ -19,9 +21,11 @@ IPT=/sbin/iptables
 IFACE="ens3"
 ADMIN="181.21.0.0/16"
 SSHPORT="44555"
-DNS_SERVER="8.8.4.4 8.8.8.8"
+DNS_SERVER="108.61.10.10"
 PACKAGE_SERVER="archive.ubuntu.com security.ubuntu.com"
+IPSET_HOSTS="104.16.37.47,104.16.38.47,104.20.4.21,104.20.5.21,138.201.14.212,151.101.4.133,185.21.103.31,188.40.39.38,199.188.221.36,208.70.186.167,209.124.55.40"
 URTPORTS="1337,1339"
+AUTH_MASTER_PORTS="27952,27900"
 
 echo "Enabling Firewall..."
 
@@ -47,10 +51,18 @@ done
 
 # Allowing repositories
 echo "Enabling Repositories..."
-for ip in $PACKAGE_SERVER
+for repositories in $PACKAGE_SERVER
 do
-	$IPT -A OUTPUT -o $IFACE -p tcp -d "$ip" -m multiport --dport 80,443 -m state --state NEW,ESTABLISHED -j ACCEPT
-	$IPT -A INPUT -i $IFACE -p tcp -s "$ip" -m multiport --sport 80,443 -m state --state ESTABLISHED -j ACCEPT
+	$IPT -A OUTPUT -o $IFACE -p tcp -d "$repositories" -m multiport --dports 80,443 -m state --state NEW,ESTABLISHED -j ACCEPT
+	$IPT -A INPUT -i $IFACE -p tcp -s "$repositories" -m multiport --sports 80,443 -m state --state ESTABLISHED -j ACCEPT
+done
+
+# Allowing IPSET hosts
+echo "Enabling IPSET hosts..."
+for hosts in $IPSET_HOSTS
+do
+	$IPT -A OUTPUT -o $IFACE -p tcp -d "$IPSET_HOSTS" -m multiport --dports 80,443 -m state --state NEW,ESTABLISHED -j ACCEPT
+	$IPT -A INPUT -i $IFACE -p tcp -s "$IPSET_HOSTS" -m multiport --sports 80,443 -m state --state ESTABLISHED -j ACCEPT
 done
 
 # Admin IPs Version 2
@@ -82,22 +94,29 @@ $IPT -A OUTPUT -o $IFACE -p tcp -d $ADMIN --sport $SSHPORT -m conntrack --ctstat
 echo "Enabling DOS SSH atack prevention... "
 $IPT -A INPUT -i $IFACE -p tcp -m tcp --dport $SSHPORT -m conntrack --ctstate NEW -m recent --set --name DEFAULT --rsource
 $IPT -A INPUT -i $IFACE -p tcp -m tcp --dport $SSHPORT -m conntrack --ctstate NEW -m recent --update --seconds 180 --hitcount 3 --name DEFAULT --rsource -j LOG -m limit --limit 20/m --log-prefix "iptables SSH Attempt on port $SSHPORT: "
-$IPT -A INPUT -i $IFACE -p tcp -m tcp --dport $SSHPORT -m conntrack --ctstate NEW -m recent --update --seconds 180 --hitcount 3 --name DEFAULT --rsource -j REJECT
+$IPT -A INPUT -i $IFACE -p tcp -m tcp --dport $SSHPORT -m conntrack --ctstate NEW -m recent --update --seconds 180 --hitcount 3 --name DEFAULT --rsource -j DROP
 $IPT -A INPUT -i $IFACE -p tcp -m tcp --dport $SSHPORT -m conntrack --ctstate NEW -j ACCEPT
 
 # Log and allow UrT
 echo "Logging and allowing UrT connections..."
-$IPT -A INPUT -i $IFACE -p udp -m multiport --dport $URTPORTS -j LOG --log-prefix "iptables: UrT INC Connections: "
-$IPT -A INPUT -i $IFACE -p udp -m multiport --dport $URTPORTS -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT
-$IPT -A OUTPUT -o $IFACE -p udp -m multiport --sport $URTPORTS -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT
+$IPT -A INPUT -i $IFACE -p udp -m multiport --dports $URTPORTS -j LOG --log-prefix "iptables: UrT Connections: "
+$IPT -A INPUT -i $IFACE -p udp -m multiport --dports $URTPORTS -j ACCEPT
+$IPT -A OUTPUT -o $IFACE -p udp -m multiport --sports $URTPORTS -j ACCEPT
+
+echo "Allowing auth and masterlist..."
+$IPT -A INPUT -i $IFACE -p udp -m multiport --sports $AUTH_MASTER_PORTS -j ACCEPT
+$IPT -A OUTPUT -o $IFACE -p udp -m multiport --dports $AUTH_MASTER_PORTS -j ACCEPT
+
+echo "Deny Gameservers"
+$IPT -A INPUT -i $IFACE -p udp --sport 27960 -j DROP
 
 # Attack prevention (only 3 attempts by an IP every 3 minutes, drop the rest)
 # The ACCEPT at the end is necessary or, it wouldn't accept any connection
 echo "Enabling UrT DOS atack prevention..."
-$IPT -A INPUT -i $IFACE -p udp -m multiport --dport $URTPORTS -m conntrack --ctstate NEW -m recent --set --name DEFAULT --rsource
-$IPT -A INPUT -i $IFACE -p udp -m multiport --dport $URTPORTS -m conntrack --ctstate NEW -m recent --update --seconds 180 --hitcount 3 --name DEFAULT --rsource -j LOG -m limit --limit 20/m --log-prefix "iptables UrT Attack on port $URTPORTS: "
-$IPT -A INPUT -i $IFACE -p udp -m multiport --dport $URTPORTS -m conntrack --ctstate NEW -m recent --update --seconds 180 --hitcount 3 --name DEFAULT --rsource -j REJECT
-$IPT -A INPUT -i $IFACE -p udp -m multiport --dport $URTPORTS -m conntrack --ctstate NEW -j ACCEPT
+$IPT -A INPUT -i $IFACE -p udp -m multiport --dports $URTPORTS -m conntrack --ctstate NEW -m recent --set --name DEFAULT --rsource
+$IPT -A INPUT -i $IFACE -p udp -m multiport --dports $URTPORTS -m conntrack --ctstate NEW -m recent --update --seconds 180 --hitcount 3 --name DEFAULT --rsource -j LOG -m limit --limit 20/m --log-prefix "iptables UrT Attack on port $URTPORTS: "
+$IPT -A INPUT -i $IFACE -p udp -m multiport --dports $URTPORTS -m conntrack --ctstate NEW -m recent --update --seconds 180 --hitcount 3 --name DEFAULT --rsource -j DROP
+$IPT -A INPUT -i $IFACE -p udp -m multiport --dports $URTPORTS -m conntrack --ctstate NEW -j ACCEPT
 
 # Drop all packets to port 111 except those from localhost
 echo "Rejecting all packets to port 111 excecpt packets from localhost... "
@@ -109,23 +128,28 @@ $IPT -A INPUT -i $IFACE -p tcp --dport 113 -j REJECT --reject-with tcp-reset
 
 # Don't log route packets coming from routers - too much logging
 echo "Rejecting router's packets..."
-$IPT -A INPUT -i $IFACE -p udp --dport 520 -j REJECT
+$IPT -A INPUT -p udp --dport 520 -j REJECT
 
 # Don't log smb/windows sharing packets - too much logging
 echo "Disabling logging smb packets..."
 $IPT -A INPUT -i $IFACE -p tcp --dport 137:139 -j REJECT
 $IPT -A INPUT -i $IFACE -p udp --dport 137:139 -j REJECT
 
-# Drop INVALID packets
-echo "Dropping INVALID packets... "
-$IPT -A INPUT -m conntrack --ctstate INVALID -j DROP
-
 # Blocking excessive syn packet
-echo "Blocking syn packets... "
+echo "Blocking syn packets..."
 $IPT -N SYN_FLOOD
 $IPT -A INPUT -p tcp --syn -j SYN_FLOOD
 $IPT -A SYN_FLOOD -m limit --limit 1/s --limit-burst 3 -j RETURN
 $IPT -A SYN_FLOOD -j DROP
+
+# Stop smurf attacks
+echo "Enabling smurf attack detector..."
+$IPT -A INPUT -i $IFACE -p icmp -m icmp --icmp-type address-mask-request -j DROP
+$IPT -A INPUT -i $IFACE -p icmp -m icmp --icmp-type timestamp-request -j DROP
+$IPT -A INPUT -i $IFACE -p icmp -m icmp -j DROP
+
+# Drop excessive RST packets to avoid smurf attacks
+$IPT -A INPUT -i $IFACE -p tcp -m tcp --tcp-flags RST RST -m limit --limit 2/second --limit-burst 2 -j ACCEPT
 
 # LOGGING
 echo "Creating LOGGING chain..."
@@ -142,19 +166,31 @@ $IPT -A LOGGING -p icmp -j REJECT --reject-with icmp-port-unreachable
 $IPT -A LOGGING -m limit --limit 1/min -j LOG --log-level 4
 $IPT -A LOGGING -j DROP
 
-# Block
-# drop reserved addresses incoming (these are reserved addresses)
-# but may change soon
-echo "Blocking reserved ip addresses... "
+# Send spoofed packers to the LOGGING chain tobe processed and then droped
+echo "Enabliing Spoof attack detector..."
+
+# These adresses are mostly used for LAN's, so if these would come to a WAN-only server, drop them
+$IPT -A INPUT -i $IFACE -s 10.0.0.0/8 -j LOGGING
+$IPT -A INPUT -i $IFACE -s 127.0.0.0/8 -j LOGGING
+$IPT -A INPUT -i $IFACE -s 169.254.0.0/16 -j LOGGING
+$IPT -A INPUT -i $IFACE -s 172.16.0.0/12 -j LOGGING
+$IPT -A INPUT -i $IFACE -s 192.168.0.0/16 -j LOGGING
+
+# Multicast-adresses
+$IPT -A INPUT -i $IFACE -s 0.0.0.0/8 -j LOGGING
+$IPT -A INPUT -i $IFACE -d 0.0.0.0/8 -j LOGGING
+$IPT -A INPUT -i $IFACE -s 224.0.0.0/4 -j LOGGING
+$IPT -A INPUT -i $IFACE -d 224.0.0.0/4 -j LOGGING
+$IPT -A INPUT -i $IFACE -s 240.0.0.0/5 -j LOGGING
+$IPT -A INPUT -i $IFACE -d 240.0.0.0/5 -j LOGGING
+$IPT -A INPUT -i $IFACE -d 239.255.255.0/24 -j LOGGING
+$IPT -A INPUT -i $IFACE -d 255.255.255.255 -j LOGGING
+
+# Reserved adresses and US comercial ips
 $IPT -A INPUT -i $IFACE -s 1.0.0.0/8 -j LOGGING
 $IPT -A INPUT -i $IFACE -s 2.0.0.0/8 -j LOGGING
 $IPT -A INPUT -i $IFACE -s 5.0.0.0/8 -j LOGGING
 $IPT -A INPUT -i $IFACE -s 7.0.0.0/8 -j LOGGING
-$IPT -A INPUT -i $IFACE -s 10.0.0.0/8 -j LOGGING
-
-# Mostly US Commercial IP space, Google Fiber, and Business ISPs
-#$IPT -A INPUT -i eth0 -s 23.0.0.0/8 -j DUMP
-echo "Blocking US commercial IP... "
 $IPT -A INPUT -i $IFACE -s 27.0.0.0/8 -j LOGGING
 $IPT -A INPUT -i $IFACE -s 31.0.0.0/8 -j LOGGING
 $IPT -A INPUT -i $IFACE -s 36.0.0.0/8 -j LOGGING
@@ -164,40 +200,13 @@ $IPT -A INPUT -i $IFACE -s 42.0.0.0/8 -j LOGGING
 $IPT -A INPUT -i $IFACE -s 58.0.0.0/8 -j LOGGING
 $IPT -A INPUT -i $IFACE -s 59.0.0.0/8 -j LOGGING
 $IPT -A INPUT -i $IFACE -s 60.0.0.0/8 -j LOGGING
-$IPT -A INPUT -i $IFACE -s 127.0.0.0/8 -j LOGGING
-$IPT -A INPUT -i $IFACE -s 169.254.0.0/16 -j LOGGING
-$IPT -A INPUT -i $IFACE -s 172.16.0.0/12 -j LOGGING
-$IPT -A INPUT -i $IFACE -s 192.168.0.0/16 -j LOGGING
 $IPT -A INPUT -i $IFACE -s 197.0.0.0/8 -j LOGGING
-$IPT -A INPUT -i $IFACE -s 224.0.0.0/3 -j LOGGING
-$IPT -A INPUT -i $IFACE -s 240.0.0.0/8 -j LOGGING
 
-# drop reserved addresses incoming (these are reserved addresses)
-# but may change soon
-$IPT -A OUTPUT -o $IFACE -d 1.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 2.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 5.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 7.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 10.0.0.0/8 -j LOGGING
-
-# Mostly US Commercial IP space, Google Fiber, and Business ISPs
-#$IPT -A OUTPUT -o eth0 -d 23.0.0.0/8 -j DUMP
-$IPT -A OUTPUT -o $IFACE -d 27.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 31.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 36.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 39.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 41.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 42.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 58.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 59.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 60.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 127.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 169.254.0.0/16 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 172.16.0.0/12 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 192.168.0.0/16 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 197.0.0.0/8 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 224.0.0.0/3 -j LOGGING
-$IPT -A OUTPUT -o $IFACE -d 240.0.0.0/8 -j LOGGING
+# Drop INVALID packets
+echo "Dropping INVALID packets... "
+$IPT -A INPUT -m conntrack --ctstate INVALID -j DROP
+$IPT -A OUTPUT -m conntrack --ctstate INVALID -j DROP
+$IPT -A FORWARD -m conntrack --ctstate INVALID -j DROP
 
 # All policies set to DROP
 echo "Setting up DROP policy..."
