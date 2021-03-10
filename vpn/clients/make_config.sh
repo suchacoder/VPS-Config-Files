02907dcb64998e6f8bc62dfbda64d834531ed407
Leandro <leanhack@hotmail.com>
2021-03-10 18:44:30 +0000

    Added vpn folder and some scripts

---
+++ vpn/clients/make_config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# First argument: Client identifier
+
+KEY_DIR=~/vpn/clients/areco/keys
+OUTPUT_DIR=~/vpn/clients/files
+BASE_CONFIG=~/vpn/clients/base.conf
+
+cat ${BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    ${KEY_DIR}/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    ${KEY_DIR}/${1}.crt \
+    <(echo -e '</cert>\n<key>') \
+    ${KEY_DIR}/${1}.key \
+    <(echo -e '</key>\n<tls-auth>') \
+    ${KEY_DIR}/ta.key \
+    <(echo -e '</tls-auth>') \
+    > ${OUTPUT_DIR}/${1}.ovpn
