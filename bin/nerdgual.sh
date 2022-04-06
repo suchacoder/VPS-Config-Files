af6db3a344bc6a647caa5ca5611dddb3c041667a
suchacoder <leanhack@hotmail.com>
2022-04-06 04:02:35 -0300

    Updated nerdgual

--- bin/nerdgual.sh
+++ bin/nerdgual.sh
@@ -4,6 +4,16 @@
 IPSET=$(which ipset)
 IPTABLES=$(which iptables)
 
+# VARs
+IFACE="eth0"
+ADMIN="181.191.143.0/32"
+SSHPORT="44555"
+URTPORT="27960,27961,27962"
+PACKAGE_SERVER="archive.ubuntu.com security.ubuntu.com"
+TCP_SERVICES="44555"
+DP_SERVICES="27960,27961,27962"
+#HTTP_PORTS="80,443"
+
 # Common definitions
 COMMENT="-m comment --comment"
 LOG="ULOG --ulog-nlgroup 1 --ulog-prefix"
@@ -14,7 +24,6 @@ DONT_LOG=""
 # This is useful for testing, where you can set the tables to REJECT
 # so that you can see if they are working immediately while testing.
 REJECT=DROP
-
 
 # iptables function
 # Used inplace of calling iptables directly or a variable pointing to iptables.
@@ -46,34 +55,25 @@ iptables() {
     for jump in "$actions"; do
       $IPTABLES $comment $table $rule --jump ${jump}
     done
-    
+
   fi
 }
-
 
 echo "Configuring netfilter:"
 
 
-# Flush old rules, custom tables and sets
-echo " * flushing old rules"
-$IPTABLES --flush
-$IPTABLES --delete-chain
-$IPSET flush
-$IPSET destroy
-
-
 # Set default policies for all three default chains
 echo " * setting default policies"
-$IPTABLES --policy INPUT ACCEPT
-$IPTABLES --policy FORWARD ACCEPT
+$IPTABLES --policy INPUT $REJECT
+$IPTABLES --policy FORWARD $REJECT
 $IPTABLES --policy OUTPUT ACCEPT
-
 
 # Create chains to reduce the number of rules each packet must traverse.
 echo " * creating custom rule chains"
-#$IPSET create blacklist hash:net family inet hashsize 16384 maxelem 500000
-#$IPSET create whitelist hash:ip
+## Only use ipset to create blacklist one time, then comment it and just restore 'ipset.restore' ##
+$IPSET create blacklist hash:net family inet hashsize 16384 maxelem 500000
 $IPSET restore -! < /home/chuck/ipset/ipset.restore
+$IPSET create whitelist hash:ip
 $IPTABLES --new-chain bad_packets
 $IPTABLES --new-chain bad_tcp_packets
 $IPTABLES --new-chain icmp_packets
@@ -121,7 +121,7 @@ iptables "$table" "--tcp-flags SYN,FIN SYN,FIN"              "Stealth scan"   "$
 iptables "$table" "--tcp-flags FIN,SYN FIN,SYN"              "Stealth scan"   "$REJECT"
 iptables "$table" "--tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE" "Bad TCP packet" "$REJECT"
 iptables "$table" "--tcp-flags FIN,RST FIN,RST"              "Bad TCP packet" "$REJECT"
-iptables "$table" "--tcp-flags FIN,ACK FIN"                  "Bad TCP packet" "$REJECT" 
+iptables "$table" "--tcp-flags FIN,ACK FIN"                  "Bad TCP packet" "$REJECT"
 iptables "$table" "--tcp-flags ACK,URG URG"                  "Bad TCP packet" "$REJECT"
 
 iptables "$table" "" "$DONT_LOG" "RETURN"
@@ -156,37 +156,22 @@ iptables "$table" "" "$DONT_LOG" "RETURN"
 #
 echo " * * creating inbound UDP packet chain"
 
-echo "* * allow OpenVPN"
-table="--append udp_inbound --protocol udp --source 0/0"
-rule="--in-interface eth0 --dport 1194 -m conntrack --ctstate NEW"
-iptables "$table" "$rule" "* allow INC OpenVPN *" "ACCEPT"
+# Immediately ban and drop a host attempting to access ports that should not be open
+table="--append udp_inbound --protocol udp"
+rule="-m multiport ! --ports $UDP_SERVICES"
+iptables "$table" "$rule" "$DONT_LOG" "SET --add-set blacklist src" "$REJECT"
 
-# Immediately ban and drop nerds attempting to access USP ports that should not be open
-nerds="-m multiport ! --ports 1194"
-iptables "$table" "$nerds" "$DONT_LOG" "SET --add-set blacklist src" "$REJECT"
-
+# Accept connections to UrT server
+subtable="$table -m multiport --destination-ports "$URTPORT""
+subrule="-m limit --limit 70/minute --limit-burst 100"
+iptables "$subtable" "$subrule" "$DONT_LOG" "ACCEPT"
 # Not matched, so return for logging
-iptables "--append udp_inbound --protocol udp" "" "$DONT_LOG" "RETURN"
+iptables "--append udp_inbound --protocol udp" "" "$LOG" "RETURN"
 
 
 # udp_outbound chain
 #
 echo " * * creating outbound UDP packet chain"
-
-table="--append udp_outbound --protocol udp --destination 0/0"
-rule="--out-interface eth0 --sport 1194 -m conntrack --ctstate ESTABLISHED,RELATED"
-iptables "$table" "$rule" "* allow OUT OpenVPN *" "ACCEPT"
-
-
-# OpenVPN Masquerade outgoing traffic
-echo " * * masquarading OpenVPN"
-
-table="-t nat"
-rule="--append POSTROUTING -o eth0 --source 10.8.0.0/24"
-iptables "$table" "$rule" "* masquerade OpenVPN *" "MASQUERADE"
-
-# Forward everything
-iptables -A FORWARD -j ACCEPT
 
 # No match, so ACCEPT
 iptables "--append udp_outbound --protocol udp" "" "$DONT_LOG" "ACCEPT"
@@ -197,23 +182,20 @@ iptables "--append udp_outbound --protocol udp" "" "$DONT_LOG" "ACCEPT"
 echo " * * creating inbound TCP packet chain"
 
 table="--append tcp_inbound --protocol tcp --source 0/0"
-
-# Web Server
-
+rule="-m multiport ! --ports $TCP_SERVICES"
+iptables "$table" "$rule" "$DONT_LOG" "SET --add-set blacklist src" "$REJECT"
 
 # SSH
-echo " * * * allowing ssh on port 4949"
+echo " * * * allowing ssh on port 44555"
 
-subtable="$table --destination-port 4949"
-
+subtable="$table --destination-port $SSHPORT"
 rule="-m recent --name SSH --update --seconds 60 --hitcount 1"
 iptables "$subtable" "$rule" "*** SSH over rate limit ***" "$REJECT"
-
 rule="-m recent --name SSH --set"
 iptables "$subtable" "$rule" "*** SSH connection attempt ***"
-
 iptables "$subtable" "" "*** SSH connection accepted ***" "ACCEPT"
 
+# Web Server
 
 # HTTP
 #echo " * * * allowing http on port 80"
@@ -228,10 +210,8 @@ iptables "$subtable" "" "*** SSH connection accepted ***" "ACCEPT"
 #rule="-m limit --limit 50/minute --limit-burst 100"
 #iptables "$subtable" "$rule" "$DONT_LOG" "ACCEPT"
 
-
 # Not matched, so return so it will be logged
 iptables "$table" "" "$DONT_LOG" "RETURN"
-
 
 
 # tcp_outbound chain
@@ -240,8 +220,6 @@ echo " * * creating outbound TCP packet chain"
 
 # No match, so ACCEPT
 iptables "--append tcp_outbound --protocol tcp --source 0/0" "" "$DONT_LOG" "ACCEPT"
-
-
 
 
 ###############################################################################
@@ -271,9 +249,6 @@ iptables "$table" "-m set --match-set whitelist src" "$DONT_LOG" "ACCEPT"
 # Drop blacklisted hosts right away
 iptables "$table" "-m set --match-set blacklist src" "$DONT_LOG" "$REJECT"
 
-# Immediately ban and drop a host attempting to access ports that should not be open
-iptables "$table --protocol tcp" "-m multiport ! --ports 4949" "$DONT_LOG" "SET --add-set blacklist src" "$REJECT"
-
 # Route the rest to the appropriate user chain
 $IPTABLES $table --protocol tcp --jump tcp_inbound
 $IPTABLES $table --protocol udp --jump udp_inbound
@@ -282,10 +257,23 @@ $IPTABLES $table --protocol icmp --jump icmp_packets
 # Log packets that still don't match
 iptables "$table" "-m limit --limit 3/minute --limit-burst 3" "Packet died"
 
-
 # Save settings
-#$(which ipset) save > $HOME/ipset/ipset.restore
-$(which iptables-save) > /home/chuck/iptables_saved/firegual.rules
+#$(which ipset) save > /home/chuck/ipset/ipset.restore
+$(which iptables-save) > /home/chuck/iptables_saved/nerdgual.rules
 
-# Woraround cuz ipset restore ain't workin' for me
+# Woraround cuz for some nerd reason ipset restore ain't workin' for me #
 #for ip in `sed -i '1d;s/add\ blacklist\ //g' /home/chuck/ipset/ipset.restore`; do ipset add blacklist $ip ; done
+
+# Script to block IPs reading a file, same scheme might be used for $blacklist or $whitelist IPs
+#if [ -f geo-ip-block.txt ]
+#then
+#        for BLOCKED_IP in 'cat geo-ip-block.txt'
+#        do
+#                iptables -A INPUT -s $BLOCKED_IP -j DROP
+#        done
+#else
+#        echo "No Geo-IP Blocking file exists"
+#fi
+
+## Uncomment to test new firewall rules
+#sleep 60 && sh -c /home/chuck/bin/killgual.sh
